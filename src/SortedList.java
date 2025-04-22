2605cdb8cda98b46bb9b1ee96faf8681e92b3687
K-NotKansas <ohioyasirk@gmail.com>
2025-04-21 23:28:46 -0400

    main

---
+++ src/SortedList.java
@@ -0,0 +1,18 @@
+public class SortedList {
+    public static int searchDataSet(String[] toSearch, String searchFor) {
+        int left = 0;
+            int right = toSearch.length - 1;
+        while (left <= right) {
+            int center = left + (right - left) / 2;
+                if (toSearch[center].equals(searchFor)) {
+                    return center;
+                }
+                if (toSearch[center].compareTo(searchFor) < 0) {
+                    left = center + 1;
+                } else {
+                    right = center - 1;
+                }
+        }
+        return 400;
+    }
+}
